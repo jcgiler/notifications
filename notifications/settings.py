da1fb40da7c44b5db9d5493493bcc8ff8b5b8f9f
jcgiler <jcgiler@in-planet.net>
2016-06-22 19:29:45 -0500

    Modify settings.py in notifications

--- notifications/settings.py
+++ notifications/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'notifications.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'notify',
+        'USER': 'notify',
+        'PASSWORD': '6a63670d0a',
+        'HOST': 'localhost'
     }
 }
 
@@ -104,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-EC'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
 
