71b7c0383dc35721679edcceaf0b655f45e512a5
Juan Carlos Giler <jcgiler@localhost.localdomain>
2016-06-24 11:04:18 -0500

    odoo.py modified

--- lib/api/odoo.py
+++ lib/api/odoo.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python2
 
-import xmlrpclib as Client
+import erppeek
 
-url = 'http://openerp.in-planet.net:8069'
-db = 'IN_PLANET'
-user = 'admin'
-password = '@dminKoru90'
+SERVER = 'http://openerp.in-planet.net:8069'
+DATABASE = 'IN_PLANET'
+USERNAME = 'admin'
+PASSWORD = '@dminKoru90'
 
-common = Client.ServerProxy('%s/xmlrpc/2/common' % url)
-common.version()
+client = erppeek.Client(SERVER, DATABASE, USERNAME, PASSWORD)
+
+#partner = client.read(
+#        'res.partner',
+#        [('customer', '=', 'True')],
+#        ('id', 'name', 'vat_id')
+#    )
+
+contract = client.read(
+        'ipnt.contract',
+        [('state', 'in', ('active','finalized')), ('id', '=', '11142')],
+        ('partner_id', 'id_sisbase', 'state'),
+    )
+
+print contract
